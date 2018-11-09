af8fc2d349fe1affa917d0d48b6e903fd35f6885
developerathome <radojicicprogramer@gmail.com>
2018-11-09 22:00:22 +0100

    added-login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public showLoginPage: boolean = true;
+  public showMainPage: boolean = false;
+  public loginUsername: string = 'admin';
+  public loginPassword: string = '123';
   title = 'task1';
+
+  public CheckLoginData(data: object){
+    let username = data['username'];
+    let password = data['password'];
+    if(username === this.loginUsername && password === this.loginPassword){
+      this.showLoginPage = false;
+      this.showMainPage = true;
+    }
+  }
 }
